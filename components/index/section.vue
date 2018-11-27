bfc18e57885b89764a9d3c48b2a57a66de99949d
ilin chang <niliyzhang@foxmail.com>
2018-11-27 12:14:34 +0800

    首页左侧板块

---
+++ components/index/section.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+section
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,9 @@
 		  <el-col :span="5">
 		  	<index-menu />
 		  </el-col>
-		  <el-col :span="19"><div>222</div></el-col>
+		  <el-col :span="19">
+		  	<index-life/>
+		  </el-col>
 		</el-row>
 		<el-row>
 		  <el-col :span="24"><div>666</div></el-col>
@@ -15,10 +17,12 @@
 <script>
 
 import indexMenu from '@/components/index/menu.vue'
+import indexLife from '@/components/index/life'
 
 export default {
   components: {
   	indexMenu,
+  	indexLife,
   }
 }
 </script>
