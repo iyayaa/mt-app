0a91fd87f01e77b751710896f04aada3e6d52e73
ilin chang <niliyzhang@foxmail.com>
2018-12-02 21:09:16 +0800

    全品列表页-面包屑导航

---
+++ pages/products.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-row class="page-product">
+    <el-col :span="19">
+		<crumbs :keyword="keyword"/>
+    </el-col>
+    <el-col :span="5">2
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+
+import Crumbs from '@/components/products/crumbs.vue'
+
+
+  export default {
+    components:{
+	  Crumbs,
+	},
+	data(){
+      return {
+        keyword:'波龙',
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  @import '@/assets/css/products/index.scss'
+</style>
