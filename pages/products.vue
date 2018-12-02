c6d58d30fd87a5fe7ff0f0771ecf5f81bd6d1338
ilin chang <niliyzhang@foxmail.com>
2018-12-02 21:33:19 +0800

    全品列表页-分类模块

--- pages/products.vue
+++ pages/products.vue
@@ -2,6 +2,7 @@
   <el-row class="page-product">
     <el-col :span="19">
 		<crumbs :keyword="keyword"/>
+		<categroy :types="types" :areas="areas"/>
     </el-col>
     <el-col :span="5">2
     </el-col>
@@ -11,15 +12,43 @@
 <script>
 
 import Crumbs from '@/components/products/crumbs.vue'
+import Categroy from '@/components/products/categroy.vue'
 
 
   export default {
     components:{
 	  Crumbs,
+	  Categroy,
 	},
 	data(){
       return {
         keyword:'波龙',
+        types:[{
+        	type:'娱乐',
+        	module:['杨幂','唱歌','浮动','操作','唱歌','浮动','操作']
+        },{
+        	type:'娱乐',
+        	module:['杨幂','唱歌']
+        },{
+        	type:'娱乐',
+        	module:['杨幂','唱歌']
+        },{
+        	type:'娱乐',
+        	module:['杨幂','唱歌']
+        },{
+        	type:'娱乐',
+        	module:['杨幂','唱歌']
+        },{
+        	type:'娱乐',
+        	module:['杨幂','唱歌']
+        },],
+        areas:[{
+        	type:'健身',
+        	module:['瑜伽','体操']
+        },{
+        	type:'娱乐',
+        	module:['杨幂','唱歌']
+        }],
       }
     },
   }
